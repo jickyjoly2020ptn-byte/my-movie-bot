f950809b5f9eabbd94746714ff04a4480b86f0b1
jickyjoly2020ptn-byte <jickyjoly2020ptn@gmail.com>
2026-07-24 02:31:24 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import certifi
 
 # === သတ်မှတ်ချက်များ တန်းထည့်ရန် ===
 BOT_TOKEN = "8905382319:AAFJ31OsrSFUGvZC331dCz2dAJ_0RVwND7U"  # သင့် Bot Token
-WEBAPP_URL = "https://my-movie-bot-2-vsdp.onrender.com"  # သင့် Render Web URL
+WEBAPP_URL = "https://my-movie-bot-1-ftp0.onrender.com"  # သင့် Render Web URL
 ADMIN_IDS = [2043111276]  # သင့် Telegram User ID (ဂဏန်းသက်သက်)
 MONGO_URI = "mongodb+srv://botuser:<noVWoqQYdwDk5e4A>@cluster0.uiuftrc.mongodb.net/?appName=Cluster0"  # သင့် MongoDB URI
 
